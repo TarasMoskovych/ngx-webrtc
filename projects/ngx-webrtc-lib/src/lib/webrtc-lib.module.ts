5c633bda8b351554bac4d2fb76b9f47548bd940b
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-09-02 13:21:58 +0300

    adjust configs

---
+++ projects/ngx-webrtc-lib/src/lib/webrtc-lib.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { WebrtcComponent } from './webrtc.component';
+
+@NgModule({
+  declarations: [
+    WebrtcComponent
+  ],
+  imports: [
+  ],
+  exports: [
+  ]
+})
+export class WebrtcModule { }
