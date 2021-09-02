c9a52ca866595c5fb144518d5586b0699e7631c7
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-09-02 13:02:15 +0300

    init lib

---
+++ projects/ngx-webrtc-lib/src/lib/ngx-webrtc-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-webrtc-lib',
+  template: `
+    <p>
+      ngx-webrtc-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxWebrtcLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
