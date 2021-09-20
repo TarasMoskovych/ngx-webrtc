62eec0eb668f715a419275e51ea6a7e133f90549
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-09-20 16:21:28 +0300

    move keys to environment file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { WebRtcModule } from 'projects/ngx-webrtc-lib/src/public-api';
-
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    WebRtcModule.forRoot({ AppID: '' }),
+    WebRtcModule.forRoot(environment.configs),
   ],
   providers: [],
   bootstrap: [AppComponent]
