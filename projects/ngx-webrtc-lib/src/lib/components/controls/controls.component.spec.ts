ff26c684577e77a2fc4ed19095930569f1dd323f
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-09-27 19:30:00 +0300

    create controls component

---
+++ projects/ngx-webrtc-lib/src/lib/components/controls/controls.component.spec.ts
@@ -0,0 +1,57 @@
+import { ControlsComponent } from './controls.component';
+
+describe('ControlsComponent', () => {
+  let component: ControlsComponent;
+
+  beforeEach(() => {
+    component = new ControlsComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('onToggleCamera', () => {
+    beforeEach(() => {
+      spyOn(component.toggleCamera, 'emit');
+    });
+
+    it('should emit event', () => {
+      component.onToggleCamera();
+      expect(component.toggleCamera.emit).toHaveBeenCalledWith(!component.cameraEnabled);
+    });
+  });
+
+  describe('onToggleMicrophone', () => {
+    beforeEach(() => {
+      spyOn(component.toggleMicrophone, 'emit');
+    });
+
+    it('should emit event', () => {
+      component.onToggleMicrophone();
+      expect(component.toggleMicrophone.emit).toHaveBeenCalledWith(!component.microphoneEnabled);
+    });
+  });
+
+  describe('onToggleFullScreen', () => {
+    beforeEach(() => {
+      spyOn(component.toggleFullScreen, 'emit');
+    });
+
+    it('should emit event', () => {
+      component.onToggleFullScreen();
+      expect(component.toggleFullScreen.emit).toHaveBeenCalledWith(!component.fullScreenEnabled);
+    });
+  });
+
+  describe('onEndCall', () => {
+    beforeEach(() => {
+      spyOn(component.endCall, 'emit');
+    });
+
+    it('should emit event', () => {
+      component.onEndCall();
+      expect(component.endCall.emit).toHaveBeenCalledWith();
+    });
+  });
+});
