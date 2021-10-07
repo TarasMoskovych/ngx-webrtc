09e745b5449f99a21eb3f2b7246c3a967732b1b2
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-10-07 11:49:28 +0300

    create components for host app for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 import { WebRtcModule } from 'projects/ngx-webrtc-lib/src/public-api';
 import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
+import { ConferenceComponent, HomeComponent } from './components';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'conference',
+    component: ConferenceComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConferenceComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     WebRtcModule.forRoot(environment.configs),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
