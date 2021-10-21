5ebf602d700295609561b3c13b8ac3fa32477056
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-10-21 10:18:16 +0300

    add unit tests for host app

--- src/app/components/conference/conference.component.spec.ts
+++ src/app/components/conference/conference.component.spec.ts
@@ -1,25 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConferenceComponent } from './conference.component';
 
 describe('ConferenceComponent', () => {
   let component: ConferenceComponent;
-  let fixture: ComponentFixture<ConferenceComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ConferenceComponent ]
-    })
-    .compileComponents();
-  });
+  let router: jasmine.SpyObj<Router>;
+  let route: jasmine.SpyObj<ActivatedRoute>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ConferenceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    route = jasmine.createSpyObj('ActivatedRoute', [], {
+      snapshot: {
+        queryParams: {
+          channelId: undefined,
+        },
+      },
+    });
+    component = new ConferenceComponent(route, router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should set channel', () => {
+      component.ngOnInit();
+      expect(component.channel).toBe('test-channel');
+    });
+  });
+
+  describe('onCallEnd', () => {
+    it('should redirect to home page', () => {
+      component.onCallEnd();
+      expect(router.navigateByUrl).toHaveBeenCalledOnceWith('/');
+    });
   });
 });
--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,25 +1,67 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+  let formBuilder: jasmine.SpyObj<FormBuilder>;
+  let router: jasmine.SpyObj<Router>;
+  let localStorage: jasmine.SpyObj<Storage>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    formBuilder = jasmine.createSpyObj('FormBuilder', ['group']);
+    router = jasmine.createSpyObj('Router', ['navigate']);
+    localStorage = jasmine.createSpyObj('Storage', ['getItem', 'setItem']);
+
+    component = new HomeComponent(formBuilder, router, localStorage);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('get channelId', () => {
+    it('should return default channelId', () => {
+      localStorage.getItem.and.returnValue(null);
+      expect(component.channelId).toBe('test-channel-ngx-webrtc');
+    });
+
+    it('should return channelId taken from local storage', () => {
+      const channelId = 'test-channel';
+      localStorage.getItem.and.returnValue(channelId);
+
+      expect(component.channelId).toBe(channelId);
+    });
+  });
+
+  describe('ngOnInit', () => {
+    it('should create form using form builder', () => {
+      component.ngOnInit();
+      expect(formBuilder.group).toHaveBeenCalled();
+    });
+  });
+
+  describe('onSubmit', () => {
+    const formData = {
+      channelId: 'test-channel',
+    };
+
+    beforeEach(() => {
+      formBuilder.group.and.returnValue({ value: formData } as FormGroup);
+      component.ngOnInit();
+      component.onSubmit();
+    });
+
+    it('should save channelId to local storage', () => {
+      expect(localStorage.setItem).toHaveBeenCalledOnceWith('ngx-webrtc:channelId', formData.channelId);
+    });
+
+    it('should redirect to conference with query params', () => {
+      expect(router.navigate).toHaveBeenCalledOnceWith(
+        ['conference'],
+        { queryParams: { ...formData } },
+      );
+    });
   });
 });
