3e9422bdae22e951f4072c0244935d42431fc17b
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2021-10-28 09:59:30 +0300

    install library from npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
-import { WebRtcModule } from 'projects/ngx-webrtc-lib/src/public-api';
 import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { ConferenceComponent, HomeComponent } from './components';
-import { ReactiveFormsModule } from '@angular/forms';
+
+// import { WebRtcModule } from 'projects/ngx-webrtc-lib/src/public-api';
+import { WebRtcModule } from 'ngx-webrtc-lib';
 
 const routes: Routes = [
   {
