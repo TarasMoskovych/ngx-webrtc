c49ec4a26274b11ba3fd2588e1e517d9e533a681
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2022-01-13 16:45:58 +0200

    implement dialog service

---
+++ projects/ngx-webrtc-lib/src/lib/services/dialog.service.spec.ts
@@ -0,0 +1,55 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { DialogComponent } from '../components';
+
+import { DialogService } from './dialog.service';
+
+@Component({
+  template: '<p>Test</p>',
+})
+class TestComponent extends DialogComponent {
+  @Input() channel: string;
+}
+
+describe('DialogService', () => {
+  const channel = 'test-channel';
+  let service: DialogService;
+  let ngDocument: Document;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+    service = TestBed.inject(DialogService);
+    ngDocument = TestBed.inject(DOCUMENT);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('open', () => {
+    it('should create a component and attach it inside the wrapper with props', () => {
+      const component = service.open(TestComponent, { channel });
+      expect((component as TestComponent).channel).toBe(channel);
+    });
+
+    it('should create only one single wrapper for dialog', () => {
+      service.open(TestComponent);
+      service.open(TestComponent);
+      service.open(TestComponent);
+
+      expect(ngDocument.querySelectorAll('.ngx-webrtc-wrapper').length).toBe(1);
+    });
+
+    it('should destroy the component on "afterClosed"', fakeAsync(() => {
+      const instance = service.open(TestComponent);
+
+      spyOn(service['appRef'], 'detachView');
+      instance.afterClosed.emit();
+      tick(500);
+
+      expect(service['appRef'].detachView).toHaveBeenCalled();
+    }));
+  });
+});
---
+++ projects/ngx-webrtc-lib/src/lib/services/dialog.service.ts
@@ -0,0 +1,70 @@
+import { DOCUMENT } from '@angular/common';
+import { ApplicationRef, ComponentFactoryResolver, EmbeddedViewRef, Inject, Injectable, Injector, Type } from '@angular/core';
+import { delay, take } from 'rxjs/operators';
+import { DialogComponent } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+  private wrapperSelector = 'ngx-webrtc-wrapper';
+
+  constructor(
+   @Inject(DOCUMENT) private document: Document,
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector,
+  ) { }
+
+  open(component: Type<any>, data: any = {}): DialogComponent { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return this.appendComponent(component, data);
+  }
+
+  private appendComponent(component: Type<any>, data: any): DialogComponent { // eslint-disable-line @typescript-eslint/no-explicit-any
+    // 1. Create a component reference from the component
+    const componentRef = this.componentFactoryResolver
+      .resolveComponentFactory(component)
+      .create(this.injector);
+
+    const { instance } = componentRef;
+
+    // Set inputs
+    Object.keys(data).forEach((key: string) => instance[key] = data[key]);
+
+    // 2. Attach incoming component to the appRef so that it's inside the ng component tree
+    this.appRef.attachView(componentRef.hostView);
+
+    // 3. Get DOM element from component
+    const domElem = (componentRef.hostView as EmbeddedViewRef<any>) // eslint-disable-line @typescript-eslint/no-explicit-any
+      .rootNodes[0] as HTMLElement;
+
+    // 4. Append DOM element to the container
+    this.getContainer().append(domElem);
+
+    // 5. Destroy after fade out
+    (instance as DialogComponent).afterClosed
+      .pipe(
+        take(1),
+        delay(250),
+      )
+      .subscribe(() => {
+        this.appRef.detachView(componentRef.hostView);
+        componentRef.destroy();
+      });
+
+    return instance as DialogComponent;
+  }
+
+  private getContainer(): HTMLElement {
+    let container: HTMLElement = this.document.querySelector(`.${this.wrapperSelector}`) as HTMLElement;
+
+    if (!container) {
+      container = this.document.createElement('div');
+      container.setAttribute('class', this.wrapperSelector);
+      container.setAttribute('style', 'position: fixed; top: 0; width: 100vw;');
+      this.document.body.appendChild(container);
+    }
+
+    return container;
+  }
+}
