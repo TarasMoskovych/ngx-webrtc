4330d50c36748ea0261dbc22f6905b00ec586c96
Taras_Moskovych <ICTaras.Moskovych@amwell.com>
2022-02-14 14:09:25 +0200

    improve call ended event

--- projects/ngx-webrtc-lib/src/lib/models/dialog.model.ts
+++ projects/ngx-webrtc-lib/src/lib/models/dialog.model.ts
@@ -1,6 +1,6 @@
 import { Subject } from 'rxjs';
 
 export interface DialogComponent {
-  afterClosed: Subject<any>; // eslint-disable-line @typescript-eslint/no-explicit-any
-  closeDialog: (data?: any) => void; // eslint-disable-line @typescript-eslint/no-explicit-any
+  afterClosed: Subject<any>;
+  closeDialog: (data?: any) => void;
 }
