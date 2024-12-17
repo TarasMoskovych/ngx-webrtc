a871078c88ede9e69a779ae2b2d270a4cd8faef4
Taras Moskovych <ictaras.moskovych@amwell.com>
2024-12-17 15:58:35 +0200

    bump 18.2.0

--- src/app/ngx-webrtc-lib.ts
+++ src/app/ngx-webrtc-lib.ts
@@ -1,3 +1,13 @@
+// import {
+//   provideWebRtc,
+//   User,
+//   VideoCallDialog,
+//   VideoCallDialogData,
+//   VideoCallDialogService,
+//   WebRtcComponent,
+//   WebRtcModule,
+// } from '@projects/ngx-webrtc-lib/src/public-api';
+
 import {
   provideWebRtc,
   User,
@@ -6,16 +16,7 @@ import {
   VideoCallDialogService,
   WebRtcComponent,
   WebRtcModule,
-} from '@projects/ngx-webrtc-lib/src/public-api';
-
-// import {
-//   VideoCallDialog,
-//   VideoCallDialogData,
-//   VideoCallDialogService,
-//   WebRtcComponent,
-//   WebRtcModule,
-//   provideWebRtc,
-// } from 'ngx-webrtc-lib';
+} from 'ngx-webrtc-lib';
 
 export {
   provideWebRtc,
